6d1ca2de031599c4584d9558f956c23f1fbf4b75
eldad-assaf <eldadux@gmail.com>
2023-10-15 15:46:33 +0300

    name error

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,27 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors/index");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  UserNameError,
+} = require("../errors/index");
 
 const register = async (req, res) => {
+  const name = req.body.name;
+  if ((name && name.length < 3) || name.length > 20) {
+    throw new UserNameError("Username must be between 3-20 characters");
+  }
 
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
-  
+
   res
     .status(StatusCodes.CREATED)
-    .json({ user: { name: user.name , email : user.email }, token: token, id: user.id });
+    .json({
+      user: { name: user.name, email: user.email },
+      token: token,
+      id: user.id,
+    });
 };
 
 const login = async (req, res) => {
@@ -19,20 +31,24 @@ const login = async (req, res) => {
     throw new BadRequestError();
   }
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
   if (!user) {
     throw new UnauthenticatedError();
   }
 
-  const isPasswordCorrect = await  user.comparePassword(password);
-  if(!isPasswordCorrect){
-    throw new UnauthenticatedError('Invalid Credentials')
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
   }
   const token = user.createJWT();
 
   res
     .status(StatusCodes.OK)
-    .json({ user: { name: user.name , email :user.email }, token: token, id: user.id });
+    .json({
+      user: { name: user.name, email: user.email },
+      token: token,
+      id: user.id,
+    });
 };
 
-module.exports = { register,login };
+module.exports = { register, login };
